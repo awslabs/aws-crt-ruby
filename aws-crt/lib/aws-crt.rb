e4d04bf2959954cbcfb52dd974fe61e21fcf5d9d
Alex Woods <alexwoo@amazon.com>
2020-08-26 13:00:41 -0700

    Use FFI and bundle a library with correct platforms.

--- aws-crt/lib/aws-crt.rb
+++ aws-crt/lib/aws-crt.rb
@@ -1,27 +1,55 @@
-# Must be defined before require
+require 'ffi'
+
+# Maps platform to crt binary name.  Needs to match what is used in the Rakefile for builds
+PLATFORMS = {
+  "universal-darwin" => 'libaws-c-common.dylib'
+}.freeze
+
+def host_string
+  "#{host_cpu}-#{host_os}"
+end
+
+# @return [String] host cpu, even on jruby
+def host_cpu
+  case RbConfig::CONFIG["host_cpu"]
+  when /86_64/
+    "x86_64"
+  when /86/
+    "x86"
+  else
+    RbConfig::CONFIG["host_cpu"]
+  end
+end
+
+# @return [String] host os, even on jruby
+def host_os
+  case RbConfig::CONFIG["host_os"]
+  when /darwin/
+    "darwin"
+  when /linux/
+    "linux"
+  when /mingw|mswin/
+    "mingw32"
+  else
+    RbConfig::CONFIG["host_os"]
+  end
+end
+
+platform = PLATFORMS.keys.find { |p| Gem::Platform.new(p) === Gem::Platform.new(host_string) }
+COMMON_BIN_PATH = File.expand_path("../bin/#{platform}/#{PLATFORMS[platform]}", File.dirname(__FILE__))
+
 module Aws
   module Crt
-    class MyMalloc
-      VERSION = "1.0"
+    extend FFI::Library
+    ffi_lib [COMMON_BIN_PATH, 'libaws-c-common']
+    attach_function :aws_high_res_clock_get_ticks, [ :pointer ], :int
+
+    def self.my_time
+      p = FFI::MemoryPointer.new(:uint64)
+      aws_high_res_clock_get_ticks(p)
+      # p.read(:uint64)  # read is not supported in jruby version?
+      p.get_uint64(0)
     end
   end
 end
 
-require "aws-crt/aws_crt"
-
-require 'ffi'
-
-COMMON_BIN_PATH = File.expand_path("../bin/libaws-c-common", File.dirname(__FILE__))
-
-module CRT
-  extend FFI::Library
-  ffi_lib [COMMON_BIN_PATH, 'libaws-c-common']
-  attach_function :aws_high_res_clock_get_ticks, [ :pointer ], :int
-
-  def self.mytime
-    p = FFI::MemoryPointer.new(:uint64)
-    CRT::aws_high_res_clock_get_ticks(p)
-    # p.read(:uint64)  # read is not supported in jruby version?
-    p.get_uint64(0)
-  end
-end
