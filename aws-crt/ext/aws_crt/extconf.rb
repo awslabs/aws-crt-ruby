2dc27f26744ccb8e3789b9d4639d814cc236775b
Alex Woods <alexwoo@amazon.com>
2020-08-26 13:04:59 -0700

    Remove unused extconf file

--- aws-crt/ext/aws_crt/extconf.rb
+++
@@ -1,8 +0,0 @@
-require 'mkmf'
-
-# Check for whatever we need to build our ext here
-abort "missing malloc()" unless have_func "malloc"
-abort "missing free()"   unless have_func "free"
-
-create_header
-create_makefile('aws_crt')
