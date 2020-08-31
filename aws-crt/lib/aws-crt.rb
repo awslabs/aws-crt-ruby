64fe889dc1c398a6d9be003c0602da1df0b8398f
Michael Graeb <graebm@amazon.com>
2020-08-31 16:36:21 -0700

    Split up ruby code across multiple files. (#8)

--- aws-crt/lib/aws-crt.rb
+++ aws-crt/lib/aws-crt.rb
@@ -1,89 +1,30 @@
-require 'ffi'
+module Aws
+  module Crt
+  end
+end
+
 require_relative 'aws-crt/platforms'
+require_relative 'aws-crt/native'
+require_relative 'aws-crt/errors'
+require_relative 'aws-crt/io'
 
 module Aws
   module Crt
-    extend FFI::Library
+    # Ensure native init() is called when gem loads
+    Aws::Crt::Native.init
 
-    ffi_lib [crt_bin_path(local_platform), 'libaws-crt']
-
-    # Core API
-    attach_function :aws_crt_init, [], :void
-    attach_function :aws_last_error, [], :int
-    attach_function :aws_error_str, [:int], :string
-    attach_function :aws_error_name, [:int], :string
-    attach_function :aws_error_lib_name, [:int], :string
-    attach_function :aws_error_debug_str, [:int], :string
-    # IO API
-    attach_function :aws_crt_event_loop_group_new, [:uint16], :pointer
-    attach_function :aws_crt_event_loop_group_destroy, [:pointer], :void
-    # Internal testing API
-    attach_function :aws_crt_test_error, [], :int
-    attach_function :aws_crt_test_pointer_error, [], :pointer
-
-    # Ensure aws_crt_init is called on library load
-    aws_crt_init
-
+    # Invoke native call, and raise exception if it failed
     def self.call
       res = yield
+      # functions that return void cannot fail
       return unless res
+
+      # for functions that return int, non-zero indicates failure
       Errors.raise_last_error if res.is_a?(Integer) && res != 0
+
+      # for functions that return pointer, NULL indicates failure
       Errors.raise_last_error if res == FFI::Pointer::NULL
       res
-    end
-
-    # Base class for CRT Errors
-    class Error < StandardError
-    end
-
-    module Errors
-      @const_set_mutex = Mutex.new
-
-      def self.raise_last_error
-        error_code = Aws::Crt.aws_last_error
-        error_name = Aws::Crt.aws_error_name(error_code)
-        raise error_class(error_name), Aws::Crt.aws_error_debug_str(error_code)
-      end
-
-      # Get the error class for a given error_name
-      def self.error_class(error_name)
-        constant = error_class_constant(error_name)
-        if error_const_set?(constant)
-          # modeled error class exist
-          # set code attribute
-          err_class = const_get(constant)
-          err_class
-        else
-          set_error_constant(constant)
-        end
-      end
-
-      private
-
-      # Convert an error code to an error class name/constant.
-      # This requires filtering non-safe characters from the constant
-      # name and ensuring it begins with an uppercase letter.
-      def self.error_class_constant(error_name)
-        constant = error_name.to_s.gsub(/AWS_ERROR_/, '').split('_').map(&:capitalize).join
-      end
-
-      def self.set_error_constant(constant)
-        @const_set_mutex.synchronize do
-          # Ensure the const was not defined while blocked by the mutex
-          if error_const_set?(constant)
-            const_get(constant)
-          else
-            error_class = Class.new(Aws::Crt::Error)
-            const_set(constant, error_class)
-          end
-        end
-      end
-
-      def self.error_const_set?(constant)
-        # Purposefully not using #const_defined? as that method returns true
-        # for constants not defined directly in the current module.
-        constants.include?(constant.to_sym)
-      end
     end
   end
 end
---
+++ aws-crt/lib/aws-crt/native.rb
@@ -0,0 +1,23 @@
+require 'ffi'
+
+module Aws::Crt
+  module Native
+    extend FFI::Library
+
+    ffi_lib [crt_bin_path(local_platform), 'libaws-crt']
+
+    # Core API
+    attach_function :init, :aws_crt_init, [], :void
+    attach_function :last_error, :aws_last_error, [], :int
+    attach_function :error_str, :aws_error_str, [:int], :string
+    attach_function :error_name, :aws_error_name, [:int], :string
+    attach_function :error_lib_name, :aws_error_lib_name, [:int], :string
+    attach_function :error_debug_str, :aws_error_debug_str, [:int], :string
+    # IO API
+    attach_function :event_loop_group_new, :aws_crt_event_loop_group_new, [:uint16], :pointer
+    attach_function :event_loop_group_destroy, :aws_crt_event_loop_group_destroy, [:pointer], :void
+    # Internal testing API
+    attach_function :test_error, :aws_crt_test_error, [], :int
+    attach_function :test_pointer_error, :aws_crt_test_pointer_error, [], :pointer
+  end
+end
