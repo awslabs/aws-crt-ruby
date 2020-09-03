1e9c8d164293ba6e97e52f25751aac1f2ef9177e
Alex Woods <alexwoo@amazon.com>
2020-09-03 08:57:47 -0700

    Fix spacing

--- aws-crt/native/src/errors.c
+++ aws-crt/native/src/errors.c
@@ -7,21 +7,21 @@
 #include <aws/common/error.h>
 
 int aws_crt_last_error(void) {
-  return aws_last_error();
+    return aws_last_error();
 }
 
 const char *aws_crt_error_str(int err) {
-  return aws_error_str(err);
+    return aws_error_str(err);
 }
 
 const char *aws_crt_error_name(int err) {
-  return aws_error_name(err);
+    return aws_error_name(err);
 }
 
 const char *aws_crt_error_debug_str(int err) {
-  return aws_error_debug_str(err);
+    return aws_error_debug_str(err);
 }
 
 void aws_crt_reset_error(void) {
-  aws_reset_error();
+    aws_reset_error();
 }
