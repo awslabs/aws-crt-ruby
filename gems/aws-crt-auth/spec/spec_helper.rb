05809dd7ce08b0b1e6708d8a327c581a2b203d81
Alex Woods <alexwoo@amazon.com>
2020-09-15 12:20:06 -0700

    Drop 2.3 from CI config

--- gems/aws-crt-auth/spec/spec_helper.rb
+++ gems/aws-crt-auth/spec/spec_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+
+# use the local version of aws-crt
 $LOAD_PATH.unshift File.expand_path('../../aws-crt/lib', __dir__)
 
 puts $LOAD_PATH
