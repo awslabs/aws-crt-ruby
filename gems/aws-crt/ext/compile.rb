757bc5641c528179afaac3ce1db6609078197ba1
Michael Graeb <graebm@amazon.com>
2020-09-21 22:59:03 +0000

    Revert "Get Linux CI working:"
    This reverts commit ea82ab049febf339560953aed6947929db9d27d8.

--- gems/aws-crt/ext/compile.rb
+++ gems/aws-crt/ext/compile.rb
@@ -5,56 +5,41 @@ require 'fileutils'
 require 'shellwords'
 require_relative '../lib/aws-crt/platforms'
 
-CMAKE = find_executable('cmake3') || find_executable('cmake')
-abort 'Missing cmake' unless CMAKE
+abort 'Missing cmake' unless find_executable 'cmake'
 
 def cmake_version
-  version_str = `#{CMAKE} --version`
+  version_str = `cmake --version`
   match = /(\d+)\.(\d+)\.(\d+)/.match(version_str)
   [match[1].to_i, match[2].to_i, match[3].to_i]
 end
 
-CMAKE_VERSION = cmake_version
-
 # whether installed cmake supports --parallel build flag
 def cmake_has_parallel_flag?
-  (CMAKE_VERSION <=> [3, 12]) >= 0
+  (cmake_version <=> [3, 12]) >= 0
 end
 
 def run_cmd(args)
-  cmd = Shellwords.join(args)
-  puts cmd
-  system(cmd) || raise("Error running: #{cmd}")
+  system(*args) || raise("Error running: #{Shellwords.join(args)}")
 end
 
 # Compile bin to expected location
 def compile_bin
-  platform = local_platform
-  native_dir = File.expand_path('../native', File.dirname(__FILE__))
-  build_dir = File.expand_path('../tmp', File.dirname(__FILE__))
-  bin_dir = crt_bin_dir(platform)
+  FileUtils.chdir(File.expand_path('..', File.dirname(__FILE__))) do
+    platform = local_platform
+    native_dir = File.expand_path('./native')
+    build_dir = File.expand_path('./tmp')
+    bin_dir = crt_bin_dir(platform)
+    config_cmd = ['cmake', native_dir, "-DBIN_DIR=#{bin_dir}"]
+    build_cmd = ['cmake', '--build', build_dir, '--target', 'aws-crt']
+    build_cmd.append('--parallel') if cmake_has_parallel_flag?
 
-  config_cmd = [CMAKE, native_dir, "-DBIN_DIR=#{bin_dir}"]
-
-  # if LIBCRYPTO_PATH set in ENV, pass it to cmake via CMAKE_PREFIX_PATH
-  libcrypto_path = ENV['LIBCRYPTO_PATH']
-  if libcrypto_path
-    # if libcrypto_path is relative, make it absolute, since we run
-    # cmake from a different directory
-    libcrypto_path = File.absolute_path(libcrypto_path)
-
-    config_cmd.append("-DCMAKE_PREFIX_PATH=#{libcrypto_path}")
-  end
-
-  build_cmd = [CMAKE, '--build', build_dir, '--target', 'aws-crt']
-  build_cmd.append('--parallel') if cmake_has_parallel_flag?
-
-  # Need to run cmake from build dir.
-  # Later versions of cmake (3.13+) can pass build dir via -B,
-  # but min supported cmake (3.1) does not support this.
-  FileUtils.mkdir_p(build_dir)
-  FileUtils.chdir(build_dir) do
-    run_cmd(config_cmd)
-    run_cmd(build_cmd)
+    # Need to run cmake from build dir.
+    # Later versions of cmake (3.13+) can pass build dir via -B,
+    # but min supported cmake (3.1) does not support this.
+    FileUtils.mkdir_p(build_dir)
+    FileUtils.chdir(build_dir) do
+      run_cmd(config_cmd)
+      run_cmd(build_cmd)
+    end
   end
 end
