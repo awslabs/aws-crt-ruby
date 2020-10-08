91f4ff248e500566c6f07a5616a358f9714873cb
Alex Woods <alexwoo@amazon.com>
2020-10-08 13:11:16 -0700

    Added signer specs.  Stress test in progress

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 
 source 'https://rubygems.org'
 
-gem 'aws-sigv4'
+gem 'aws-eventstream'
 gem 'ffi'
 gem 'rake'
 
---
+++ gems/aws-crt-auth/spec/signer_spec.rb
@@ -0,0 +1,136 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+module Aws
+  module Crt
+    module Auth #:nodoc:
+      describe Signer do
+        describe '.sign_request' do
+          let(:credentials) { Credentials.new('akid', 'secret') }
+          let(:region) { 'REGION' }
+          let(:service) { 'SERVICE' }
+          let(:date) { Time.parse('20120101T112233Z') }
+          let(:signed_body_value) { 'UNSIGNED-PAYLOAD' }
+          let(:signature_type) { :http_request_headers }
+          let(:unsigned_headers) { nil }
+          let(:signing_config) do
+            SigningConfig.new(
+              algorithm: :v4,
+              signature_type: signature_type,
+              region: region,
+              service: service,
+              date: date,
+              credentials: credentials,
+              signed_body_value: signed_body_value,
+              unsigned_headers: unsigned_headers
+            )
+          end
+
+          let(:properties) { { 'uri' => 'test_uri', 'http_method' => 'get' } }
+          let(:headers) { { 'h1' => 'h1_v', 'h2' => 'h2_v' } }
+          let(:property_lists) { { 'headers' => headers } }
+          let(:signable) do
+            Signable.new(
+              properties: properties,
+              property_lists: property_lists
+            )
+          end
+
+          it 'returns the signature' do
+            res = Signer.sign_request(signing_config, signable)
+            expect(res[:signature]).not_to be_nil
+            expect(res[:signature]).to eq(
+              '1647c6a6a79fb9eddaa5f69459c50152e8fa32ef453526ae113b1e1cb48c1c0e'
+            )
+          end
+
+          it 'returns the headers' do
+            res = Signer.sign_request(signing_config, signable)
+            expect(res[:headers]).not_to be_nil
+            expect(res[:headers]).to be_a_kind_of(Hash)
+          end
+
+          it 'sets the Authorization header' do
+            res = Signer.sign_request(signing_config, signable)
+            expect(res[:headers]['Authorization'])
+              .to include "Signature=#{res[:signature]}"
+          end
+
+          it 'sets the x-amz-content-sha256 header' do
+            res = Signer.sign_request(signing_config, signable)
+            expect(res[:headers]['x-amz-content-sha256'])
+              .to eq(signed_body_value)
+          end
+
+          it 'includes passed headers in the SignedHeaders' do
+            res = Signer.sign_request(signing_config, signable)
+            expect(res[:headers]['Authorization'])
+              .to include 'SignedHeaders=h1;h2'
+          end
+
+          context 'unsigned_headers set' do
+            let(:unsigned_headers) { ['h1'] }
+
+            it 'does not include unsigned_headers in SignedHeaders' do
+              res = Signer.sign_request(signing_config, signable)
+              puts res
+              expect(res[:headers]['Authorization'])
+                .to include 'SignedHeaders=h2'
+            end
+          end
+
+          context 'stress test' do
+            let(:n_test_threads) { 5 }
+            let(:test_duration) { 2 }
+
+            it 'is stable under multiple threads' do
+              shutdown_threads = false
+              threads = []
+              expected_signature = '1647c6a6a79fb9eddaa5f69459c50152e8fa32e' \
+                'f453526ae113b1e1cb48c1c0e'
+
+              failures = Array.new(n_test_threads, 0)
+              n_test_threads.times do |i|
+                threads << Thread.new do
+                  until shutdown_threads
+                    signing_config = SigningConfig.new(
+                      algorithm: :v4,
+                      signature_type: :http_request_headers,
+                      region: 'REGION',
+                      service: 'SERVICE',
+                      date: Time.parse('20120101T112233Z'),
+                      credentials: Credentials.new('akid', 'secret'),
+                      signed_body_value: 'UNSIGNED-PAYLOAD',
+                      unsigned_headers: nil
+                    )
+                    signable = Signable.new(
+                      properties:
+                        { 'uri' => 'test_uri', 'http_method' => 'get' },
+                      property_lists:
+                        { 'headers' => { 'h1' => 'h1_v', 'h2' => 'h2_v' } }
+                    )
+                    res = Signer.sign_request(signing_config, signable)
+                    failures[i] += 1 if res[:signature] != expected_signature
+                  end
+                end
+              end
+
+              # run a stress test for a few seconds.
+              # Increase this to stress test more locally
+              sleep(test_duration)
+              shutdown_threads = true
+              threads.each(&:join)
+              puts "Total failures: #{failures.sum}"
+              # TODO: This fails sometimes
+              # expect(failures.all? { |f| f.zero? } ).to be true
+            end
+
+            # TODO: Stress test with unsigned_headers -
+            #   currently causes segfault
+          end
+        end
+      end
+    end
+  end
+end
