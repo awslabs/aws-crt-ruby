9b5dd9ab75d367fd71893fa9052f7243619a9fb7
Alex Woods <alexwoo@amazon.com>
2020-10-15 15:25:11 -0700

    Remove more from the spec helper

--- gems/aws-sigv4/spec/spec_helper.rb
+++ gems/aws-sigv4/spec/spec_helper.rb
@@ -2,13 +2,9 @@
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
-# use the local version of aws-crt libs
-$LOAD_PATH.unshift File.expand_path('../../aws-crt/lib', __dir__)
-$LOAD_PATH.unshift File.expand_path('../../aws-crt-auth/lib', __dir__)
-
 require_relative '../../aws-crt/spec/spec_helper'
-require 'rspec'
 require 'aws-sigv4'
+
 #
 # module SpecHelper
 #   class << self
