50f811c815386a2792bbc0f3bbf2f3e7c9fe8a9a
Michael Graeb <graebm@amazon.com>
2021-07-30 15:10:51 -0700

    different compile process

--- gems/aws-crt/ext/compile.rb
+++ gems/aws-crt/ext/compile.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'etc'
 require 'mkmf'
 require 'fileutils'
 require 'shellwords'
@@ -30,36 +31,53 @@ def run_cmd(args)
   system(*args) || raise("Error running: #{cmd_str}")
 end
 
+def find_file(name, search_dirs, base_dir)
+  search_dirs.each do |search_dir|
+    dir = File.expand_path(search_dir, base_dir)
+    file_path = File.expand_path(name, dir)
+    return file_path if File.exist?(file_path)
+  end
+  raise "Cannot find #{name}"
+end
+
 # Compile bin to expected location
 def compile_bin
   platform = local_platform
-  native_dir = File.expand_path('../native/crt/aws-crt-ffi',
-                                File.dirname(__FILE__))
-  build_dir = File.expand_path('../tmp', File.dirname(__FILE__))
-  bin_dir = crt_bin_dir(platform)
-  install_dir = File.expand_path(build_dir, 'install')
+  native_dir = File.expand_path('../aws-crt-ffi', File.dirname(__FILE__))
+  tmp_build_dir = File.expand_path('../tmp/build', File.dirname(__FILE__))
+
+  # We need cmake to "install" aws-crt-ffi so that the binaries end up in a
+  # predictable location. But cmake still adds subdirectories we don't want,
+  # so we'll "install" under tmp, and manually copy to bin/ after that.
+  tmp_install_dir = File.expand_path('../tmp/install', File.dirname(__FILE__))
 
   config_cmd = [
-    CMAKE, native_dir,
-    "-DCMAKE_INSTALL_PREFIX=#{install_dir}"
+    CMAKE,
+    "-H#{native_dir}",
+    "-B#{tmp_build_dir}",
+    "-DCMAKE_INSTALL_PREFIX=#{tmp_install_dir}"
   ]
 
-  build_cmd = [CMAKE, '--build', build_dir, '--target', 'aws-crt-ffi']
-  build_cmd.append('--parallel') if cmake_has_parallel_flag?
+  build_cmd = [CMAKE, '--build', tmp_build_dir, '--target', 'install']
 
-  # Need to run cmake from build dir.
-  # Later versions of cmake (3.13+) can pass build dir via -B,
-  # but min supported cmake (3.1) does not support this.
-  FileUtils.mkdir_p(build_dir)
-  FileUtils.mkdir_p(bin_dir)
-  FileUtils.chdir(build_dir) do
-    run_cmd(config_cmd)
-    run_cmd(build_cmd)
-    # Temporary fix for windows bin locations
-    if platform.os.include? 'mingw32'
-      FileUtils.mv("Debug/#{crt_bin_name(platform)}", bin_dir)
-    else
-      FileUtils.mv(crt_bin_name(platform), bin_dir)
-    end
+  # Build using all processors
+  if cmake_has_parallel_flag?
+    build_cmd.append('--parallel')
+    build_cmd.append(Etc.nprocessors.to_s)
   end
+
+  run_cmd(config_cmd)
+  run_cmd(build_cmd)
+
+  # Move file to bin/, instead of where cmake installed it under tmp/
+  bin_dir = crt_bin_dir(platform)
+  FileUtils.mkdir_p(bin_dir)
+  bin_name = crt_bin_name(platform)
+  search_dirs = [
+    'bin', # windows
+    'lib64', # some 64bit unix variants
+    'lib', # some unix variants
+  ]
+  tmp_path = find_file(bin_name, search_dirs, tmp_install_dir)
+  FileUtils.cp(tmp_path, bin_dir)
 end
