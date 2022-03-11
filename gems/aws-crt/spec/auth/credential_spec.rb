c876e548982b41e98111362fa042549d8037ebee
Itamar Levy-Or <ilevyor@amazon.com>
2022-03-11 17:00:45 -0500

    rubocop fixes

--- gems/aws-crt/spec/auth/credential_spec.rb
+++ gems/aws-crt/spec/auth/credential_spec.rb
@@ -5,7 +5,7 @@ require 'weakref'
 
 module Aws
   module Crt
-    module Auth #:nodoc:
+    module Auth # :nodoc:
       describe Credentials do
         describe '#initilize' do
           it 'raises an ArgumentError when missing access_key_id' do
